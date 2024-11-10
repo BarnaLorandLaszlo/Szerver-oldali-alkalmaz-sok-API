ca7de071bc86d1c3ca266229e0b11b08cc994114
Barna Loránd László <traktor.mtz1970@gmail.com>
2024-11-10 18:55:39 +0100

    Call an ASP.NET Core web API with JavaScript

--- TodoApi/obj/Debug/net8.0/TodoApi.AssemblyInfo.cs
+++ TodoApi/obj/Debug/net8.0/TodoApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6606d0d8cbac068c8dfdfe02d46d26eed1af682f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+823c7b8c8f41c4540eb85dad671aa152f99ce5c8")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
